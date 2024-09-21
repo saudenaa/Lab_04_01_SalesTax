1b4f28e7a69d2a9bdee8f1018b576d93e739e77a
saudenaa <saudenaa@mail.uc.edu>
2024-09-21 07:58:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    // setting literal price
+    double purchasePrice = 58;
+    double salesTaxRate = 0.05; // sales tax rate  5%
+    double salesTax = purchasePrice * salesTaxRate;
+
+    //Output of the sales tax
+         System.out.println("Purchase Price "+" = "+purchasePrice);
+         System.out.println("Sales tax rate "+" = "+salesTaxRate);
+         System.out.println("The sales tax is "+salesTax);
+
+
+    }
+}
+
